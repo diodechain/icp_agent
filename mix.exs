84948953cbc035ea324dccb0ddd2f91bd0c64747
Dominic Letz <dominic@diode.io>
2025-01-15 18:54:27 +0100

    Added hex info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,30 @@
 defmodule ICPAgent.MixProject do
   use Mix.Project
 
+  @url "https://github.com/diodechain/icp_agent"
   def project do
     [
       app: :icp_agent,
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "ICPAgent is an Elixir agent for the Internet Computer (ICP).",
+      package: [
+        licenses: ["Apache-2.0"],
+        maintainers: ["Dominic Letz"],
+        links: %{"GitHub" => @url}
+      ],
+      # Docs
+      name: "Candid",
+      source_url: @url,
+      docs: [
+        # The main page in the docs
+        main: "Candid",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -26,7 +43,8 @@ defmodule ICPAgent.MixProject do
       {:cbor, "~> 1.0"},
       {:jason, "~> 1.4"},
       {:ex_sha3, "~> 0.1.1"},
-      {:req, "~> 0.5.8"}
+      {:req, "~> 0.5.8"},
+      {:ex_doc, "~> 0.25.0", only: :dev}
     ]
   end
 end
