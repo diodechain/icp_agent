0e094681d547a7d0f30e0d8a23cbeb1ae3b132ca
Dominic Letz <dominic@diode.io>
2025-01-17 13:10:35 +0100

    Fix elixir version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ICPAgent.MixProject do
     [
       app: :icp_agent,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
