2f10979dcb9b9383c9d43b6d7f46f03d242b34e1
Dominic Letz <dominic@diode.io>
2025-01-20 17:21:37 +0100

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ICPAgent.MixProject do
   def project do
     [
       app: :icp_agent,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
